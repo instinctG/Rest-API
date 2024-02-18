e1cc3f21d428a1eca2f24d6ba06e82fed91a22ea
cyber <aziz.sabyrbaev@bk.ru>
2024-02-18 14:21:30 +0300

    Structuring our application

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,23 @@
-package server
+package main
+
+import "fmt"
+
+// App - the struct wjich contains things like
+// pointers to database connections
+type App struct {
+}
+
+// Run - handles the startup of our application
+func (app *App) Run() error {
+	fmt.Println("Setting up our App")
+	return nil
+}
+
+// our main entrypoint for the application
+func main() {
+	app := App{}
+	if err := app.Run(); err != nil {
+		fmt.Println("Error Starting Up")
+		fmt.Println(err)
+	}
+}
